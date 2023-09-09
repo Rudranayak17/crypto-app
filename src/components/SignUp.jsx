5a011aeb9408ed9eec4541c554f4cd4a6297e2b1
Rudranayak17 <122798243+Rudranayak17@users.noreply.github.com>
2023-09-09 22:58:36 +0530

    first commit

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,35 +1,61 @@
-import { Avatar, Button, Container, Heading, Input, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {
+  Avatar,
+  Button,
+  Container,
+  Heading,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import {  AiOutlineGoogle} from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-   <Container maxW={"container.xl"}  h={"120vh"} p={"16"}>
-    <form>
-        <VStack alignItems={"stretch"} spacing={"8"} w={["full","96"]} m={"auto"} my={"16"}>
-<Heading textAlign={'center'}>Xcrypto</Heading>
-<Avatar alignSelf={"center"} boxSize={'32'}/>
-<Input placeholder='Name' type='text' focusBorderColor='purple.500' required />
-<Input placeholder='Email' type='email' focusBorderColor='purple.500' required />
-<Input placeholder='Password' type='password' focusBorderColor='purple.500' required />
+    <Container maxW={"container.xl"} h={"full"} p={"16"}>
+      <form>
+        <VStack
+          alignItems={"stretch"}
+          spacing={"8"}
+          w={["full", "96"]}
+          m={"auto"}
+          my={"16"}
+        >
+          <Heading textAlign={"center"}>Xcrypto</Heading>
+          <Avatar alignSelf={"center"} boxSize={"32"} />
+          <Input
+            placeholder="Name"
+            type="text"
+            focusBorderColor="purple.500"
+            required
+          />
+          <Input
+            placeholder="Email"
+            type="email"
+            focusBorderColor="purple.500"
+            required
+          />
+          <Input
+            placeholder="Password"
+            type="password"
+            focusBorderColor="purple.500"
+            required
+          />
 
-
-
-
-
-
-<Button colorScheme='purple'>Sign Up</Button>
-<Text textAlign={'center'}>Already Exist Sign up?{" "}<Button variant={'link'} colorScheme='purple'>
-    <Link to={"/login"}>Login Now</Link>
-</Button>  </Text>
-
+          <Button colorScheme="purple">Sign Up</Button>
+          <Text textAlign={"center"}>
+            Already Exist Sign up?{" "}
+            <Button variant={"link"} colorScheme="purple">
+              <Link to={"/login"}>Login Now</Link>
+            </Button>{" "}
+          </Text>
+          <Text textAlign={'center'}>OR</Text>
+          <Button colorScheme="teal"><AiOutlineGoogle/> Google</Button>
         </VStack>
-    </form>
-   </Container>
-  )
-}
+      </form>
+    </Container>
+  );
+};
 
-
-
-
-export default SignUp
+export default SignUp;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_10Dm19qCkMMmV_i14Fg2dGMjSd4UpC4",
+  authDomain: "xcrypto-3833e.firebaseapp.com",
+  projectId: "xcrypto-3833e",
+  storageBucket: "xcrypto-3833e.appspot.com",
+  messagingSenderId: "875734883999",
+  appId: "1:875734883999:web:f508f6a9b961667de78caa"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
